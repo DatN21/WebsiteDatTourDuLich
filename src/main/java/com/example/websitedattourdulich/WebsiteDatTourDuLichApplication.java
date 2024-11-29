08e73c14dea0c44640a8e5108e7dcf62dd138f70
DatN21 <126938691+DatN21@users.noreply.github.com>
2024-11-29 15:27:48 +0700

    Khoi tao du an ban dau

---
+++ src/main/java/com/example/websitedattourdulich/WebsiteDatTourDuLichApplication.java
@@ -0,0 +1,13 @@
+package com.example.websitedattourdulich;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteDatTourDuLichApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsiteDatTourDuLichApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/websitedattourdulich/WebsiteDatTourDuLichApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.websitedattourdulich;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteDatTourDuLichApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
